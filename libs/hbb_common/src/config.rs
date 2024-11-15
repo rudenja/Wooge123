82d4b2122d50e2817ac537a187554b25dbaa36fc
Wooge <308515050@qq.com>
2024-11-15 20:18:13 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["43.143.229.81"];
-pub const PUBLIC_RS_PUB_KEY: &str = "1234";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["129.226.71.148"];
+pub const PUBLIC_RS_PUB_KEY: &str = "zgPkWCDPZJyTcZ0Yh35l+z3J0oamt6HeOICWEeI+RN0=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
